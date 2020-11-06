a29f92f8b84d169ae029806c5509475fcdbfcd59
itvlog <lixiaomi613@163.com>
2020-11-06 20:52:57 +0800

    sdf

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    script: 'app.js',
+    watch: '.'
+  }],
+  deploy: {
+    production: {
+      user: 'root',                     
+      host: ['10.12.51.139'],  
+      port:'30022',         
+      ref: 'origin/main',           
+      repo: 'git@github.com:itvlog/render.git',    
+      path: '/apps/www', 
+      "post-deploy": 'npm install && pm2 reload ecosystem.config.js',  //部署后的动作
+      'pre-setup': ''
+    }
+  }
+};
